e5ff9a17e0d4020e9b7e1e6c6e02574299000ad1
swong <swong@sina.com>
2019-03-23 13:42:16 +0800

    这是第一次提交到本地库中

---
+++ src/main/java/org/swong/test/hello.java
@@ -0,0 +1,12 @@
+package org.swong.test;
+
+/**
+ * @author swong
+ * @create 2019-03-23 13:27
+ * @description
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("12344");
+    }
+}
